eb2970d881aaf3e86f9bd192b852f6e104f82ff9
AzfarTahir <mohamedazfartahir@gmail.com>
2025-06-15 22:35:31 +0800

    Updated with Bonus Challenges

--- session2/script4.js
+++ session2/script4.js
@@ -2,6 +2,33 @@ const billInput = document.getElementById("billInput");
 const tipInput = document.getElementById("tipInput");
 const result = document.getElementById("result");
 const button = document.getElementById("calculateBtn");
+const updateResult = document.getElementById("updateResult");
+
+// Both input's but be filled with valid input to enable button
+function checkInputs() {
+  button.disabled = true;
+  if (billInput.value.trim() !== "" && tipInput.value.trim() !== "") {
+    button.disabled = false;
+  }
+}
+
+billInput.addEventListener("keyup", checkInputs);
+tipInput.addEventListener("keyup", checkInputs);
+
+// Auto updates text as the user input value in the bill input
+function updateResultText() {
+  const bill = parseFloat(billInput.value);
+  const tipPercent = parseFloat(tipInput.value);
+
+  if (billInput.value.trim() !== "" && tipInput.value.trim() !== "") {
+    const tip = bill * (tipPercent / 100);
+    const total = bill + tip;
+    updateResult.textContent = "Total = RM " + total.toFixed(2);
+  }
+}
+
+billInput.addEventListener("keyup", updateResultText);
+tipInput.addEventListener("keyup", updateResultText);
 
 button.addEventListener("click", function () {
   const bill = parseFloat(billInput.value);
